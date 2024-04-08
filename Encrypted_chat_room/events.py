cdfff46afb4d6208586a8acb5c0d903e90aa4176
Luis Walls <lfwallsc@gmail.com>
2024-04-08 02:24:48 -0600

    Limit chat room to 2 users.

--- Encrypted_chat_room/events.py
+++ Encrypted_chat_room/events.py
@@ -7,19 +7,23 @@ users = {}
 
 @socketio.on("connect")
 def handle_connect():
-    print("Client connected!")
+    if len(users) == 2:
+        print(f"Client rejected")
+        return False
+    print(f"Client connected! {len(users)} Users connected: {users}")
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    users.pop(request.sid)
 
 @socketio.on("user_log_in")
 def handle_user_join(username):
     print(f"User {username} joined!")
-    users[username] = request.sid
+    users[request.sid] = username
     emit("chat", {"message": f"User {username} logged into chat", "username": username}, broadcast=True)
 
 @socketio.on("new_message")
 def handle_new_message(message):
     print(f"New message: {message}")
-    username = None 
-    for user in users:
-        if users[user] == request.sid:
-            username = user
+    username = users[request.sid]
     emit("chat", {"message": message, "username": username}, broadcast=True)
